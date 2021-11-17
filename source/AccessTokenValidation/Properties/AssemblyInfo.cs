04e9a196733aab0197482925508fff297f71f340
Ismaël Gélineau <ismael.gelineau@bimone.com>
2021-11-17 18:52:39 -0500

    a few fixes to make the build work with the new net48 framework and the new BIMOne prefix on the assembly identity.

--- source/AccessTokenValidation/Properties/AssemblyInfo.cs
+++ source/AccessTokenValidation/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright 2015 Dominick Baier, Brock Allen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,18 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Access Token Validation Middleware for IdentityServer3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccessTokenValidationMiddleware")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Dominick Baier, Brock Allen & Contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -24,3 +36,11 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("703d6b8a-e38d-4aac-a59a-1800a3686243")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
